8a8cdaa3b7331e4bbe807df874db18aa496399b9
bertapsan <bertapsan@gmail.com>
2022-12-19 20:42:31 +0100

    React router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 import App from './components/App'
 import Footer from './components/Footer'
 import './index.scss'
 
-
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <div className="container my-3" id="main">
-            <App />
+        <RouterProvider router={router} />
         </div>
         <Footer />
     </>
