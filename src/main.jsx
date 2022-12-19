679f8d42b05650a96324b8396e7375db9fb79a24
bertapsan <bertapsan@gmail.com>
2022-12-19 21:11:27 +0100

    Fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-    createBrowserRouter,
-    RouterProvider,
+    Routes, Route
   } from "react-router-dom";
 import App from './components/App'
 import Footer from './components/Footer'
 import './index.scss'
 
-const router = createBrowserRouter([
-    {
-      path: "/To-Do-List",
-      element: <App />,
-    },
-  ]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <div className="container my-3" id="main">
-        <RouterProvider router={router} />
+        <Routes>
+          <Route path='/' element={<App />}/>
+        </Routes>
         </div>
         <Footer />
     </>
