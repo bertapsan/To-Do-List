2628e950710b7ec92ebe8b27c456063a6d4d6b8f
bertapsan <bertapsan@gmail.com>
2022-12-21 18:48:22 +0100

    Restore react files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-    Routes, Route
-  } from "react-router-dom";
 import App from './components/App'
 import Footer from './components/Footer'
 import './index.scss'
@@ -12,9 +9,7 @@ import './index.scss'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <div className="container my-3" id="main">
-        <Routes>
-          <Route path='/' element={<App />}/>
-        </Routes>
+        <App />
         </div>
         <Footer />
     </>
